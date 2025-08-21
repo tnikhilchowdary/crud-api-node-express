39a7ff6347b0a562da5ddc74e8c8754d3f67e728
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-08-20 23:49:28 -0500

    MongoDB Setup

--- crud-api-node-express/server.js
+++ crud-api-node-express/server.js
@@ -1,18 +1,13 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
-
+const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the NodeJS Backend Technology, Which is more important");
-});
-
-app.listen(5002, () => {
-    console.log("Server is ruuning on port")
+  res.send("Welcome to the NodeJS Backend Technology, Which is more important");
 });
 
 mongoose.connect(
-  "mongodb+srv://admin:password@cluster0.3fst7wa.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0"
+  "mongodb+srv://tnikhilchowdary:abcd@cluster0.qnejuqh.mongodb.net/TNC?retryWrites=true&w=majority&appName=Cluster0"
 )
 .then(() => {
   console.log("✅ Connected to Database");
@@ -23,3 +18,5 @@ mongoose.connect(
 .catch((err) => {
   console.error("❌ Connection failed:", err);
 });
+
+
