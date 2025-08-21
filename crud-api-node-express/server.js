4b33e95a7b0b77fdaae9086abaefffaaec45c244
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-08-20 21:46:30 -0500

    Nodemon Js

--- crud-api-node-express/server.js
+++ crud-api-node-express/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the NodeJS Backend Technology");
+    res.send("Welcome to the NodeJS Backend Technology, Which is more important");
 });
 
-app.listen(5001, () => {
+app.listen(5002, () => {
     console.log("Server is ruuning on port")
 });
