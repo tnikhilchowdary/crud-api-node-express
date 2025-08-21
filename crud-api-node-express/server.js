4ede1d7b6f0eced4154f729ec7837de39f2a442c
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2025-08-20 22:41:46 -0500

    MongoDB Error

--- crud-api-node-express/server.js
+++ crud-api-node-express/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
 
 app.get("/", (req, res) => {
     res.send("Welcome to the NodeJS Backend Technology, Which is more important");
@@ -7,4 +9,17 @@ app.get("/", (req, res) => {
 
 app.listen(5002, () => {
     console.log("Server is ruuning on port")
+});
+
+mongoose.connect(
+  "mongodb+srv://admin:password@cluster0.3fst7wa.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0"
+)
+.then(() => {
+  console.log("✅ Connected to Database");
+  app.listen(5002, () => {
+    console.log("🚀 Server is running on port 5002");
+  });
+})
+.catch((err) => {
+  console.error("❌ Connection failed:", err);
 });
